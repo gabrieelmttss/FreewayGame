9cc14df2956136c059b2034671ac719549ceeb02
gabrieelmttss <gabrielmatos471@gmail.com>
2023-02-24 20:40:58 -0300

    adicionando mais carros

--- carro.js
+++ carro.js
@@ -1,10 +1,26 @@
-let xCarro = 460;
+//Carro 1
+let xCarro = 600;
 let yCarro = 45;
+let velocidadeCarro1 = 3;
+
+//Carro 2
+let xCarro2 = 600;
+let yCarro2 = 100;
+let velocidadeCarro2 = 4;
+
+//Carro 3
+let xCarro3 = 600;
+let yCarro3 = 155;
+let velocidadeCarro3 = 6;
 
 function mostraCarro() {
     image(imagemCarro, xCarro, yCarro, 40, 30);
+    image(imagemCarro2, xCarro2, yCarro2, 40, 30);
+    image(imagemCarro3, xCarro3, yCarro3, 40, 30);
   }
   
   function movimentaCarro() {
-    xCarro -= 3;
+    xCarro -= velocidadeCarro1;
+    xCarro2 -= velocidadeCarro2;
+    xCarro3 -= velocidadeCarro3;
   }
--- imagem.js
+++ imagem.js
@@ -2,9 +2,13 @@
 let imagemDaEstrada;
 let imagemDoAtor;
 let imagemCarro;
+let imagemCarro2;
+let imagemCarro3;
 
 function preload(){
   imagemDaEstrada = loadImage("material/estrada.png");
   imagemDoAtor = loadImage("material/ator-1.png");
   imagemCarro = loadImage("material/carro-1.png");
+  imagemCarro2 = loadImage("material/carro-2.png");
+  imagemCarro3 = loadImage("material/carro-3.png");
 }
